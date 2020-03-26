7134bd53c4c05a8d74b7deb629b988b69c141e99
bastosgabriel <bastos.gabriel@poli.ufrj.br>
2020-03-26 15:21:14 -0300

    Add safe string compare to security.authenticate()

--- rest_api_flask/restful_api/code/security.py
+++ rest_api_flask/restful_api/code/security.py
@@ -1,3 +1,4 @@
+from werkzeug.security import safe_str_cmp
 from user import User
 
 users = [
@@ -9,7 +10,7 @@ userid_mapping = {u.id: u for u in users}
 
 def authenticate(username,password):
     user = username_mapping.get(username, None)
-    if user and user.password == password:
+    if user and safe_str_cmp(user.password, password):
         return user
 
 def identity(payload):
