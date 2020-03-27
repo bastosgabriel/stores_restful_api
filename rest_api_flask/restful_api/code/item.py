46d70767c2e435a6db719156577602e295dd237a
bastosgabriel <bastos.gabriel@poli.ufrj.br>
2020-03-27 17:51:37 -0300

    Start implementation of db usage to Item and Items resources

--- rest_api_flask/restful_api/code/item.py
+++ rest_api_flask/restful_api/code/item.py
@@ -1,12 +1,21 @@
+import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 
-items = []
-
 class Items(Resource):
-
+    
     def get(self):
-        return {'Items': items}
+        connection = sqlite3.connect('store.db')
+        cursor = connection.cursor()
+
+        select_query = "SELECT * FROM items"
+        result = cursor.execute(select_query)
+        rows = result.fetchall()
+
+        connection.commit()
+        connection.close()
+
+        return {"items": rows}, 200
 
 class Item(Resource):
     parser = reqparse.RequestParser()
@@ -16,32 +25,52 @@ class Item(Resource):
         help = "This field is required!"
     )
 
+    @classmethod
+    def find_by_name(cls, name):
+        connection = sqlite3.connect('store.db')
+        cursor = connection.cursor()
+
+        select_query = "SELECT * FROM items WHERE name=?"
+        result = cursor.execute(select_query, (name,))
+
+        row = result.fetchone()
+
+        if row:
+            item = row
+        else:
+            item = None
+
+        connection.commit()
+        connection.close()
+
+        return item
+
     @jwt_required()
     def get(self, name):
-        # Search in items if new_item already exists
-        for item in items:
-            if item['name'].lower() == name.lower():
-                return item, 200
-        return {'item': None}, 404
-
+        if Item.find_by_name(name):
+            return {'item': Item.find_by_name(name)}, 200
+        else:
+            return {'item': None}, 404
+    '''
     def post(self, name):
-        
-        # Search in items if the new item already exists
-        for item in items:
-            if item['name'].lower() == name.lower():
-                return {'message': f"An item named {item['name']} already in items!"}, 400
+        if Item.find_by_name(name):
+            return {'message': f"An item named {name} already exists!"}, 400
 
         data = Item.parser.parse_args()
 
-        new_item = {
-            "name": name,
-            "price": data['price']
-        }
+        connection = sqlite3.connect('store.db')
+        cursor = connection.cursor()
 
-        items.append(new_item)
+        insert_query = "INSERT INTO items (name, price) VALUES (?, ?)"
+        result = cursor.execute(insert_query, (name, data['price']))
+        result.fetchone()
 
-        return new_item, 201
+        connection.commit()
+        connection.close()
 
+        return {"item": result}, 201
+
+    
     def put(self, name):
         data = Item.parser.parse_args()
 
@@ -66,3 +95,4 @@ class Item(Resource):
                 return items.pop(index), 200
 
         return {'message': f"Item '{name}' does not exist!"}, 400
+    '''
