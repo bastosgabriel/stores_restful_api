7c5813152362e3b15ffed2eda73d9c501cf0d52a
bastosgabriel <bastos.gabriel@poli.ufrj.br>
2020-04-11 02:49:17 -0300

    Add PostgreeSQL env variable from heroku and psycopg2

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -9,7 +11,7 @@ from resources.store import Store, Stores
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///store.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///store.db")
 
 app.secret_key = 'bobiki'
 api = Api(app)
